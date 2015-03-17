75da2392b62464edc1e7bbbe3275e212fb72f9fb
effy <effy@bityotaguests-MacBook-Pro.local>
2015-03-16 17:19:29 -0700

    added googleOnlineTask1, added new solution for searchA2DArray

--- binary_search/searchA2DArray.py
+++ binary_search/searchA2DArray.py
@@ -1,6 +1,7 @@
 __author__ = 'effy'
 '''
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following
+properties:
 
 Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
@@ -20,8 +21,10 @@ class Solution:
     # @param target, an integer
     # @return a boolean
     def searchMatrix(self, matrix, target):
+        ## start, end are indices of arrays
         start = 0
         end = len(matrix)-1
+        ##s,e are indices of integers inside arrays
         s = 0
         n = len(matrix[0])
         e = n - 1
@@ -53,5 +56,36 @@ Other thoughts:
 we can also use 0~m*n as index and convert them into [m,n] and then
 access element inside the 2D array
 '''
-a = Solution()
-print a.searchMatrix([[1]], 1)
+class Solution2:
+    '''
+    This works exactly like an flatten array
+    '''
+    def searchMatrix(self, matrix, target):
+        ## there are in total n elements in the 2D array
+        n = len(matrix)*len(matrix[0])
+        l = len(matrix[0])
+        def index_conversion(index):
+            x = index/l
+            y = index - x*l
+            return x, y
+        start = 0
+        end = n-1
+        while start <= end:
+            mid = (start+end)/2
+            x,y = index_conversion(mid)
+            current = matrix[x][y]
+            if current == target:
+                return True
+            elif current < target:
+                start = mid+1
+            else:
+                end = mid-1
+        return False
+
+
+a = Solution2()
+print a.searchMatrix([
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+], 4)
