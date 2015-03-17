48febf04472d5bf52673d188bf6758bed13892dc
effy <effy@bityotaguests-MacBook-Pro.local>
2015-03-16 18:03:45 -0700

    corrected googleTask1_sqrtX.py

--- binary_search/googleTask1_sqrtX.py
+++ binary_search/googleTask1_sqrtX.py
@@ -1,12 +1,21 @@
 __author__ = 'effy'
+'''
+Given a range of integers(inclusive), return how many square numbers are there in the range.
+e.g.:
+[4,17] returns 3. Since 4,9,16 are square numbers
+[3,17] returns 3. Same reason
+
+'''
 def solution(A,B):
     def sqrt(x):
+        if x == 0:
+            return 0
         if x == 1:
             return 1
         start = 1
         end = x/2
-        mid = (start+end)/2
-        while start < end:
+        while start <= end:
+            mid = (start + end)/2
             if mid*mid <= x and (mid+1)*(mid+1) > x:
                 return mid
             elif mid*mid > x:
@@ -23,5 +32,7 @@ def solution(A,B):
             start = 0
         else:
             start = sqrt(A)
+            if start*start < A:
+                start +=1
     return end-start+1
 print solution(4,17)
