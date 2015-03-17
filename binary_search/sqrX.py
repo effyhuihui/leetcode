b9d93f1a410d397bc77509c8b7f80db61e183174
effy <effy@bityotaguests-MacBook-Pro.local>
2015-03-16 17:55:10 -0700

    slight change on sqrX.py

--- binary_search/sqrX.py
+++ binary_search/sqrX.py
@@ -12,21 +12,22 @@ class Solution:
     # @param x, an integer
     # @return an integer
     def sqrt(self, x):
+        if x == 0:
+            return 0
         if x == 1:
             return 1
         start = 1
         end = x/2
-        mid = (start+end)/2
-        while start < end:
+        while start <= end:
+            mid = (start + end)/2
             if mid*mid <= x and (mid+1)*(mid+1) > x:
                 return mid
             elif mid*mid < x and (mid+1)*(mid+1) <= x:
                 start = mid + 1
             elif mid*mid > x:
                 end = mid - 1
-            mid = (start + end)/2
         return mid
 
 a = Solution()
-print a.sqrt(12)
+print a.sqrt(0)
 
