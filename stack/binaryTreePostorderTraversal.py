fec4e4612d83a42bafc2b83b5da64e2f5f6c9dc9
Effy Huang <effy.yao@gmail.com>
2015-03-18 11:57:46 -0700

    added way to avoid tree change for postOrderTraversal. (use preorder-like way and then reverse the result list)

--- stack/binaryTreePostorderTraversal.py
+++ stack/binaryTreePostorderTraversal.py
@@ -44,29 +44,29 @@ class Solution:
                     cur.left = None
         return res
 
+'''
+Without changing the tree
+'''
 class Solution2:
     def postorderTraversal(self, root):
         stack = []
         res = []
-        def goToMostLeft(cur):
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-        def goToMostRight(cur):
-            while cur:
-                stack.append(cur)
-                cur = cur.right
         if not root:
             return res
-        goToMostLeft(root)
+        stack.append(root)
         while stack:
-            cur = stack[-1]
-            if not cur.right:
-                stack.pop()
-                res.append(cur.val)
-            goToMostRight(cur)
-            cur.right = None
-        return res
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+        ## until here res is pre order
+        ## now we need to reverse it
+        final = []
+        while res:
+            final.append(res.pop())
+        return final
 
 
 
