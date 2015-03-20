dd02e4f12476f6206ecaf907de8dee5d70257a67
Effy Huang <effy.yao@gmail.com>
2015-03-20 13:31:27 -0700

    reviewed and simplified 3SUM_Closet

--- NSums/3SUM_Closet.py
+++ NSums/3SUM_Closet.py
@@ -3,30 +3,28 @@ Given an array S of n integers, find three integers in S such that the sum is cl
 Return the sum of the three integers. 
 You may assume that each input would have exactly one solution.
 
-    For example, given array S = {-1 2 1 -4}, and target = 1.
+	For example, given array S = {-1 2 1 -4}, and target = 1.
 
-    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+	The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 '''
 def threeSumClosest(num, target):
 	num.sort()
-	cur_diff = float("inf")
-	cur_total = 0
 	n = len(num)
+	closet = num[0] + num[1] + num[2]
 	for i in range(n-1):
-		j, k = i+1, n-1
-		while j < k:
-			total = num[i] + num[j] + num[k]
-			diff = abs(total-target)
-			if diff == 0:
-				return total
-			if diff < cur_diff:
-				cur_total = total
-				cur_diff = diff
-			if total > target:
-				k -= 1
+		start, end = i+1, n-1
+		while start < end:
+			total = num[i] + num[start] + num[end]
+			if abs(total-target) < abs(closet-target):
+				closet = total
+			if total < target:
+				start += 1
+			elif total > target:
+				end -= 1
 			else:
-				j += 1
-	return cur_total
+				closet = total
+				break
+	return closet
 
 a = threeSumClosest([1,1,1,0],-100)
 print a
