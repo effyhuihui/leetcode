1b37ee2e52c39fc766c6b170643e88331c7234bf
Effy Huang <effy.yao@gmail.com>
2015-03-23 15:13:38 -0700

    comments

--- sort/firstMissingPositive.py
+++ sort/firstMissingPositive.py
@@ -6,6 +6,9 @@ Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
 
 Your algorithm should run in O(n) time and uses constant space.
+
+
+an application of sort by swapping
 '''
 
 class Solution:
--- sort/sortColors.py
+++ sort/sortColors.py
@@ -6,7 +6,7 @@ adjacent, with the colors in the order red, white and blue.
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 
 Note:
-You are not suppose to use the library's sort function for this problem.
+sort by swapping
 '''
 class Solution:
     # @param A a list of integers
