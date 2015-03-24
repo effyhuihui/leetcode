e9fe93c6834b99f41aa7e278e7633c12f0868024
Effy Huang <effy.yao@gmail.com>
2015-03-24 11:35:25 -0700

    added linked List Cycle

---
+++ linkedList/linkedListCycle.py
@@ -0,0 +1,33 @@
+__author__ = 'effy'
+'''
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+Thought:
+Very typical and classic two pointers problem
+'''
+
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while fast:
+            if fast == slow:
+                return True
+            else:
+                slow = slow.next
+                fast = fast.next
+                if fast:
+                    fast = fast.next
+        return False
