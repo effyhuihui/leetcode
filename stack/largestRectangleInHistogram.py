7ea125bf56fa92b22dafb8e7401d5253ab136537
Effy Huang <effy.yao@gmail.com>
2015-03-26 15:28:47 -0700

    monit

--- stack/largestRectangleInHistogram.py
+++ stack/largestRectangleInHistogram.py
@@ -16,6 +16,7 @@ Way 1:
 所以最好的结果一定会被取到。每次往两边走的复杂度是O(n)，总共有n个bar，所以时间复杂度是O(n^2)
 但是数据多的时候会time limit exceeded。
 
+but Time Limit Exceeded
 
 '''
 
@@ -48,13 +49,25 @@ class Solution1:
 
 '''
 http://www.cnblogs.com/zuoyuan/p/3783993.html
-最优的解法，我们create一个stack， stack里面存的是heights的index，
-这个stack从下向上的index对应的高度是依次递增的（当然index本身也是递增的），
-pop stack里的元素直到cur height大于stack的top。
-为什么要pop呢，其实就是看在cur height之前，所能达到的最大高度以及这个最大高度跨越了多少范围，可以算出局部最优。
+重点看图解
+Actually, we can decrease the complexity by using stack to keep track of the height and start indexes.
+Compare the current height with previous one.
 
+Case 1: current > previous (top of height stack)
+Push current height and index as candidate rectangle start position.
+
+Case 2: current = previous
+Ignore.
+
+Case 3: current < previous
+keep popping out previous heights which is higher(stop until the prev height is the same or lower),
+compute the candidate rectangle with height and width
+(current index - previous index). Push the height and index to stacks.
+
+建议用另外一个图来走一遍
+(Note: it is better use another different example to walk through the steps, and you will understand it better).
 '''
-class Solution2:
+class Solution:
     # @param height, a list of integer
     # @return an integer
     # @good solution!
@@ -62,23 +75,29 @@ class Solution2:
         maxArea = 0
         stackHeight = []
         stackIndex = []
-        for i in range(len(height)):
+        l = len(height)
+        for i in range(l):
             if stackHeight == [] or height[i] > stackHeight[len(stackHeight)-1]:
-                stackHeight.append(height[i]); stackIndex.append(i)
+                stackHeight.append(height[i])
+                stackIndex.append(i)
             elif height[i] < stackHeight[len(stackHeight)-1]:
-                lastIndex = 0
+                lastIndex = None
                 while stackHeight and height[i] < stackHeight[len(stackHeight)-1]:
                     lastIndex = stackIndex.pop()
                     tempArea = stackHeight.pop() * (i-lastIndex)
-                    if maxArea < tempArea: maxArea = tempArea
-                stackHeight.append(height[i]); stackIndex.append(lastIndex)
+                    if maxArea < tempArea:
+                        maxArea = tempArea
+                stackHeight.append(height[i])
+                stackIndex.append(lastIndex)
         while stackHeight:
             tempArea = stackHeight.pop() * (len(height) - stackIndex.pop())
             if tempArea > maxArea:
                 maxArea = tempArea
         return maxArea
 
-
+'''
+my own way to solve it
+'''
 class Solution3:
     # @param height, a list of integer
     # @return an integer
