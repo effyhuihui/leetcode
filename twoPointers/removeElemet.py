38a536ce2fcd8c87f704c1865ea4f2f116460da3
Effy Huang <effy.yao@gmail.com>
2015-03-30 13:18:46 -0700

    added remove elements

--- twoPointers/removeElemet.py
+++ twoPointers/removeElemet.py
@@ -1,1 +1,30 @@
 __author__ = 'effy'
+'''
+Given an array and a value, remove all instances of that value in place and return the
+new length.
+
+The order of elements can be changed.
+It doesn't matter what you leave beyond the new length.
+'''
+
+class Solution:
+    # @param    A       a list of integers
+    # @param    elem    an integer, value need to be removed
+    # @return an integer
+    def removeElement(self, A, elem):
+        l = len(A)
+        if l == 0 :
+            return l
+        count = 0
+        for i in range(l):
+            if A[i] == elem:
+                count += 1
+            else:
+                ## move all non-elem to the end of the list
+                A[i-count] = A[i]
+        return l-count
+
+
+x = Solution()
+print x.removeElement([1,1,1,2],1)
+
