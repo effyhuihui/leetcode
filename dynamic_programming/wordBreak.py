90cf8bd1c948487565e91cf841582ee42fb4cafe
Effy Huang <effy.yao@gmail.com>
2015-04-19 17:16:37 -0700

    added word break

---
+++ dynamic_programming/wordBreak.py
@@ -0,0 +1,70 @@
+__author__ = 'effy'
+# -*- coding: utf-8 -*-
+'''
+Given a string s and a dictionary of words dict, determine if s can be segmented
+into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+这道题考察的显然不是dfs，为什么？因为这道题不需要给出如何分割的答案，只需要判断是否可以分割为字典中的单词即可。我们考虑使用动态规划，
+这个思路看代码的话不难，用python写起来也比较清晰。
+'''
+## 纯recursion会超时
+class Solution_recursion:
+    # @param s, a string
+    # @param wordDict, a set<string>
+    # @return a boolean
+    def wordBreak(self, s, wordDict):
+        if s == None:
+            return True
+        if s in wordDict:
+            return True
+        else:
+            l = len(s)
+            for i in range(l):
+                if self.wordBreak(s[:i], wordDict) and self.wordBreak(s[i:], wordDict):
+                    return True
+            return False
+
+class Solution_dp:
+    # @param s, a string
+    # @param wordDict, a set<string>
+    # @return a boolean
+    def wordBreak(self, s, wordDict):
+        l = len(s)
+        ## dp[i] represent s[0:i] is "word breakable"
+        dp = [False for i in range(l+1)]
+        dp[0] = True
+        def helper(s,wordDict):
+            l == len(s)
+            if l == 0:
+                return True
+            for i in range(l):
+                if dp[i] and s[i:] in wordDict:
+                    return True
+            return False
+        for i in range(1,l+1):
+            dp[i]=helper(s[:i], wordDict)
+        return dp[-1]
+
+
+class Solution_from_online:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    # @good coding!
+    def wordBreak(self, s, dict):
+        dp = [False for i in range(len(s)+1)]
+        dp[0] = True
+        ## check whether s[:i] is word breakable
+        for i in range(1, len(s)+1):
+            ## see whether  s[:i] is breakable from index k
+            for k in range(i):
+                if dp[k] and s[k:i] in dict:
+                    dp[i] = True
+        return dp[len(s)]
+x = Solution_dp()
+print x.wordBreak("a",[])
