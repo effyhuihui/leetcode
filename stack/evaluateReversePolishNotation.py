479e508ce82e9c1420ecde9336cff33da38b0a10
Effy Huang <effy.yao@gmail.com>
2015-05-04 14:24:21 -0700

    added erp

--- stack/evaluateReversePolishNotation.py
+++ stack/evaluateReversePolishNotation.py
@@ -1,4 +1,5 @@
 __author__ = 'effy'
+# -*- coding: utf-8 -*-
 '''
 Evaluate the value of an arithmetic expression in Reverse Polish Notation.
 
@@ -7,27 +8,33 @@ Valid operators are +, -, *, /. Each operand may be an integer or another expres
 Some examples:
   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+唯一一点需要注意的是在python中，是向下取整的。而这道题的oj是默认的c语言中的语法，所以需要在遇到'/'的时候注意一下。
 '''
 class Solution:
     # @param tokens, a list of string
     # @return an integer
     def evalRPN(self, tokens):
-        num = []
+        stack = []
         for i in tokens:
             if i in "+-*/":
-                a=int(num.pop())
-                b=int(num.pop())
+                a=int(stack.pop())
+                b=int(stack.pop())
                 if i == "+":
                     res = a+b
                 elif i =="-":
                     res = b-a
                 elif i == "/":
-                    res = b/a
+                    if a*b < 0:
+                       res = -((-b)/a)
+                    else:
+                        res = b/a
                 else:
                     res = a*b
-                num.append(res)
+                stack.append(res)
             else:
-                num.append(i)
-        return num[-1]
+                stack.append(i)
+        return int(stack[-1])
+
+
 a = Solution()
 print a.evalRPN(["4", "13", "5", "/", "+"])
