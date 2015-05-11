e87fb3672a3972a81258b46b10167f7050f23018
Effy Huang <effy.yao@gmail.com>
2015-05-11 16:38:41 -0700

    added length of last word

---
+++ uncategoried/lengthOfLastWord.py
@@ -0,0 +1,21 @@
+__author__ = 'effy'
+'''
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example,
+Given s = "Hello World",
+return 5.
+'''
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def lengthOfLastWord(self, s):
+        a = s.strip().split(' ')
+        return len(a[-1])
+
+x = Solution()
+print x.lengthOfLastWord("")
