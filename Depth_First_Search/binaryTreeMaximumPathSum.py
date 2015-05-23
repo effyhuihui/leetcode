b24d2a7e92d56c8a708c7f9b5ce53b92b65b7986
Effy Huang <effy.yao@gmail.com>
2015-05-22 20:16:44 -0700

    added'

--- Depth_First_Search/binaryTreeMaximumPathSum.py
+++ Depth_First_Search/binaryTreeMaximumPathSum.py
@@ -29,22 +29,48 @@ class Solution:
     parent的left child，右支同理
     '''
     def maxsum(self, root):
-        if root == None: return 0
-        sum = root.val
-        lmax = 0; rmax = 0
-        if root.left:
-            lmax = self.maxsum(root.left)
-            if lmax > 0:
-                sum += lmax
-        if root.right:
-            rmax = self.maxsum(root.right)
-            if rmax > 0:
-                sum += rmax
-        if sum > self.max: self.max = sum
+        if root == None:
+            return 0
+        skew_sum = root.val
+        lmax = self.maxsum(root.left)
+        rmax = self.maxsum(root.right)
+        if lmax > 0:
+            skew_sum += lmax
+        if rmax > 0:
+            skew_sum += rmax
+        if skew_sum > self.max:
+            self.max = skew_sum
         return max(root.val, max(root.val + lmax, root.val + rmax))
 
     def maxPathSum(self, root):
-        if root == None: return 0
+        if root == None:
+            return 0
         self.maxsum(root)
         return self.max
 
+class Solution_secondround:
+    # @param root, a tree node
+    # @return an integer
+    def __init__(self):
+        self.max_sum = -10000000
+    def maxPathSum(self, root):
+        def maxSkewSum(cur_node):
+            if cur_node == None:
+                return 0
+            lmax = maxSkewSum(cur_node.left)
+            rmax = maxSkewSum(cur_node.right)
+            ## skew_sum represent the sum that from left to cur_node and to cur_node right :)
+            skew_sum = cur_node.val
+            if lmax >0:
+                skew_sum += lmax
+            if rmax >0:
+                skew_sum += rmax
+            if skew_sum > self.max_sum:
+                self.max_sum = skew_sum
+            ##the path is either go with the left side, or right side, or just one node (if left and right sum are both
+            ## less than zero )
+            return max(cur_node.val,cur_node.val+lmax, cur_node.val+rmax)
+        if root == None:
+            return 0
+        maxSkewSum(root)
+        return self.max_sum
