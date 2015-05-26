679ca91fc49b52148c1a518da1e500e92e59682f
Effy Huang <effy.yao@gmail.com>
2015-05-26 16:46:04 -0700

    anagrams

--- hashMap/anagrams.py
+++ hashMap/anagrams.py
@@ -24,5 +24,22 @@ class Solution:
                 res += group
         return res
 
+
+class Solution_secondround:
+    # @param strs, a list of strings
+    # @return a list of strings
+    def anagrams(self, strs):
+        word_dict = {}
+        res = []
+        for i in strs:
+            s = ''.join(sorted(i))
+            word_dict[s] = word_dict.get(s, []) + [i]
+        for i in word_dict.keys():
+            if len(word_dict[i]) >=2:
+                res+=word_dict[i]
+        return res
+
+
 x = Solution()
+print x.anagrams(['',''])
 print x.anagrams(["apple", "plepa", "paer","pear","earp"])
