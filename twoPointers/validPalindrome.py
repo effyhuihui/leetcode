28fcd2742fbb27e30b7a464646a03fc31b9ce1b7
Effy Huang <effy.yao@gmail.com>
2015-05-27 13:50:44 -0700

    valid palindrome

--- twoPointers/validPalindrome.py
+++ twoPointers/validPalindrome.py
@@ -31,5 +31,32 @@ class Solution:
                 back -= 1
         return True
 
+
+class Solution_secondround:
+    # @param s, a string
+    # @return a boolean
+    def isPalindrome(self, s):
+        def validChar(char):
+            if char not in '0123456789' and (ord(char)<97 or ord(char)>122):
+                return
+            return True
+        new = s.lower().strip()
+        l = len(new)
+        left,right = 0, l-1
+        if not s:
+            return True
+        while left<=right:
+            while left<right and not validChar(new[left]):
+                left += 1
+            while right>left and not validChar(new[right]):
+                right -=1
+            if new[left] != new[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+
 x = Solution()
 print x.isPalindrome("a.")
