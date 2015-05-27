5d0287be0a7eb874d834cd850ae7974bf40bb2e1
Effy Huang <effy.yao@gmail.com>
2015-05-26 23:10:02 -0700

    implement strstr

--- twoPointers/implementStrstr.py
+++ twoPointers/implementStrstr.py
@@ -21,6 +21,24 @@ class Solution:
                 return i
         return -1
 
+
+class Solution_secondround:
+    # @param haystack, a string
+    # @param needle, a string
+    # @return an integer
+    def strStr(self, haystack, needle):
+        index = -1
+        l_stack = len(haystack)
+        l_needle = len(needle)
+        if haystack == needle:
+            return 0
+        for i in range(l_stack-l_needle+1):
+            if haystack[i:i+l_needle] == needle:
+                index = i
+                break
+        return index
+
+
 x = Solution()
 print x.strStr("mississippi", "issi")
 print x.strStr("", "")
