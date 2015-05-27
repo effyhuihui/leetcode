2dc92d3dc0d523cf1b2b3d1ca101382a9106463b
Effy Huang <effy.yao@gmail.com>
2015-05-26 23:03:54 -0700

    remove element

--- swap/removeElemet.py
+++ swap/removeElemet.py
@@ -24,6 +24,19 @@ class Solution:
                 A[i-count] = A[i]
         return l-count
 
+class Solution_secondround:
+    # @param    A       a list of integers
+    # @param    elem    an integer, value need to be removed
+    # @return an integer
+    def removeElement(self, A, elem):
+        count = 0
+        for i in range(len(A)):
+            if A[i] == elem:
+                count += 1
+            else:
+                A[i-count] = A[i]
+        print A
+        return len(A) - count
 
 x = Solution()
 print x.removeElement([1,1,1,2],1)
