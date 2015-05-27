04f31213c36270e53fba725918159a285ab329c8
Effy Huang <effy.yao@gmail.com>
2015-05-26 22:57:50 -0700

    remove duplicates from sorted array

---
+++ swap/removeDuplicatesfromSortedArray.py
@@ -0,0 +1,26 @@
+__author__ = 'effy'
+'''
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the new length.
+'''
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def removeDuplicates(self, nums):
+        l = len(nums)
+        start = 0
+        duplicates = 0
+        for i in range(l):
+            if i > start and nums[i] == nums[start]:
+                duplicates += 1
+            else:
+                nums[i-duplicates] = nums[i]
+                start = i
+        return l-duplicates
