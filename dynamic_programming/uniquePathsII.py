a669492e5a99d72d4ab2df80e9ac7220119ec338
Effy Huang <effy.yao@gmail.com>
2015-05-29 15:47:32 -0700

    unique paths ii

--- dynamic_programming/uniquePathsII.py
+++ dynamic_programming/uniquePathsII.py
@@ -45,3 +45,32 @@ class Solution:
                 else:
                     ways[i][j] = 0
         return ways[-1][-1]
+
+
+class Solution_secondround:
+    # @param obstacleGrid, a list of lists of integers
+    # @return an integer
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m,n=len(obstacleGrid), len(obstacleGrid[0])
+        dp = [ [0 for i in range(n)] for j in range(m)]
+        if obstacleGrid[0][0] == 1:
+            return 0
+        else:
+            dp[0][0] = 1
+        for i in range(1,n):
+            if obstacleGrid[0][i] != 1:
+                dp[0][i] = dp[0][i-1]
+            else:
+                dp[0][i] = 0
+        for i in range(1,m):
+            if obstacleGrid[i][0] != 1:
+                dp[i][0] = dp[i-1][0]
+            else:
+                dp[i][0] = 0
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
