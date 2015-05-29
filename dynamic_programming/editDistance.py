049ce8ea5c4506b9c0cd5375556b019fa525c084
Effy Huang <effy.yao@gmail.com>
2015-05-29 16:36:40 -0700

    edit distance

--- dynamic_programming/editDistance.py
+++ dynamic_programming/editDistance.py
@@ -38,4 +38,27 @@ class Solution:
         for i in range(1,m+1):
             for j in range(1,n+1):
                 dp[i][j]=min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+(0 if word1[i-1]==word2[j-1] else 1))
+                print dp
         return dp[-1][-1]
+
+
+class Solution_secondround:
+    # @return an integer
+    def minDistance(self, word1, word2):
+        n,m = len(word1),len(word2)
+        dp = [ [0 for i in range(n+1)] for j in range(m+1)]
+        for i in range(n+1):
+            dp[0][i] = i
+        for i in range(m+1):
+            dp[i][0] = i
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+
+                if word1[j-1] == word2[i-1]:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]+1,dp[i][j-1]+1)
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1],dp[i-1][j-1])+1
+        return dp[-1][-1]
+
+x = Solution()
+print x.minDistance('sea','ate')
