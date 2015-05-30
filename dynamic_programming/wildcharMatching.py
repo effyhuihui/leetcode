d448bc586c7e265a81a4aef2c3b3ad5c28cc9e3f
Effy Huang <effy.yao@gmail.com>
2015-05-30 11:15:17 -0700

    added wildchar matching

--- dynamic_programming/wildcharMatching.py
+++ dynamic_programming/wildcharMatching.py
@@ -62,3 +62,30 @@ class Solution4:
                     return True
                 s = s[1:]
             return self.isMatch(s, p[1:])
+
+
+class Solution_ac:
+    # @param s, an input string
+    # @param p, a pattern string
+    # @return a boolean
+    def isMatch(self, s, p):
+        p_ptr, s_ptr, last_s_ptr, last_p_ptr = 0, 0, -1, -1
+        while s_ptr < len(s):
+            if p_ptr < len(p) and (s[s_ptr] == p[p_ptr] or p[p_ptr] == '?'):
+                s_ptr += 1
+                p_ptr += 1
+            elif p_ptr < len(p) and p[p_ptr] == '*':
+                p_ptr += 1
+                last_s_ptr = s_ptr
+                last_p_ptr = p_ptr
+            elif last_p_ptr != -1:
+                last_s_ptr += 1
+                s_ptr = last_s_ptr
+                p_ptr = last_p_ptr
+            else:
+                return False
+
+        while p_ptr < len(p) and p[p_ptr] == '*':
+            p_ptr += 1
+
+        return p_ptr == len(p)
