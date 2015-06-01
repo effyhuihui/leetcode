8caaf9660e33406e7982142ae6ca18aac1b93754
Effy Huang <effy.yao@gmail.com>
2015-06-01 12:36:22 -0700

    added interleaving string

--- dynamic_programming/interleavingString.py
+++ dynamic_programming/interleavingString.py
@@ -41,6 +41,54 @@ class Solution:
         return dp[-1][-1]
 
 
+
+class Solution_secondround_recur:
+    # @param s1, a string
+    # @param s2, a string
+    # @param s3, a string
+    # @return a boolean
+    def isInterleave(self, s1, s2, s3):
+        l1, l2, l3 = len(s1),len(s2),len(s3)
+        if l1+l2 != l3:
+            return False
+        charlist12, charlist3 = sorted(list(s1)+list(s2)), sorted(list(s3))
+        if charlist12 != charlist3:
+            return False
+        if l1 <=1 or l2 <= 1:
+            if s1+s2 == s3 or s2+s1 == s3:
+                return True
+            else:
+                return False
+        for i in range(1,l1):
+            for j in range(1,l2):
+                if self.isInterleave(s1[i:],s2[j:], s3[i+j:]) and self.isInterleave(s1[:i], s2[:j], s3[:i+j]):
+                    return True
+        return False
+
+
+class Solution_secondround:
+    # @param s1, a string
+    # @param s2, a string
+    # @param s3, a string
+    # @return a boolean
+    def isInterleave(self, s1, s2, s3):
+        l1, l2, l3 = len(s1),len(s2),len(s3)
+        if l1 +l2 != l3:
+            return False
+        charlist12, charlist3 = sorted(list(s2+s1)), sorted(list(s3))
+        if charlist3 != charlist12:
+            return False
+        dp = [[False for i in range(l1+1)] for j in range(l2+1)]
+        dp[0][0] = True
+        for i in range(1,l1+1):
+            dp[0][i] = dp[0][i-1] and s1[i-1] == s3[i-1]
+        for i in range(1,l2+1):
+            dp[i][0] = dp[i-1][0] and s2[i-1] == s3[i-1]
+        for i in range(1,l2+1):
+            for j in range(1,l1+1):
+                dp[i][j] = (dp[i][j-1] and s1[j-1] == s3[i+j-1]) or (dp[i-1][j] and s2[i-1]==s3[i+j-1])
+        return dp[-1][-1]
+
 x = Solution()
 print x.isInterleave("aabcc", "dbbca", "aadbbbaccc")
 
