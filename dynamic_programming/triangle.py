e054d0de64e42f32b7367fe60242d7633401cded
Effy Huang <effy.yao@gmail.com>
2015-06-01 15:11:46 -0700

    added triangle

--- dynamic_programming/triangle.py
+++ dynamic_programming/triangle.py
@@ -38,6 +38,24 @@ class Solution:
         return min(path_map[height-1])
 
 
+class Solution_secondround:
+    # @param triangle, a list of lists of integers
+    # @return an integer
+    def minimumTotal(self, triangle):
+        dp = [i[:] for i in triangle]
+        mintotal = 1000000
+        for i in range(1,len(triangle)):
+            for j in range(len(triangle[i])):
+                if j != 0 and j != len(triangle[i])-1:
+                    dp[i][j] += min(dp[i-1][j], dp[i-1][j-1])
+                elif j == 0:
+                    dp[i][j] += dp[i-1][j]
+                else:
+                    dp[i][j] += dp[i-1][j-1]
+        for i in dp[-1]:
+            mintotal = min(i,mintotal)
+        return mintotal
+
 
 x = Solution()
 a = [
