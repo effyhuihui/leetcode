4691a83f702925f6894ee14bbd794f4558211e76
Effy Huang <effy.yao@gmail.com>
2015-06-02 12:13:41 -0700

    add and search word

--- Datastructure/addAndSearchWord.py
+++ Datastructure/addAndSearchWord.py
@@ -72,3 +72,48 @@ print wordDictionary.search(".a")
 #print wordDictionary.search("bad")
 #print wordDictionary.search(".ad")
 #print wordDictionary.search("b..")
+'''
+secondround
+'''
+class TrieNode:
+    # Initialize your data structure here.
+    def __init__(self):
+        self.isWord = False
+        self.children = {}
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    # @param {string} word
+    # @return {void}
+    # Inserts a word into the trie.
+    def insert(self, word):
+        level_parent = self.root
+        for i in word:
+            if i not in level_parent.children:
+                node = TrieNode()
+                level_parent.children[i] = node
+            level_parent = level_parent.children[i]
+        level_parent.isWord = True
+
+
+    def search(self,word):
+        parent = self.root
+        for i in word:
+            if i not in parent.children:
+                return False
+            else:
+                parent = parent.children[i]
+        return parent.isWord
+
+    def startsWith(self,prefix):
+        parent = self.root
+        for i in prefix:
+            if i in parent.children:
+                parent = parent.children[i]
+            else:
+                return False
+        return True
