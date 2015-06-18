951b88769dbb9608e8ed5dabf88b52de2157939b
Effy Huang <effy.yao@gmail.com>
2015-06-18 11:33:13 -0700

    surroudned reagions

--- Breadth_First_Search/surroundedRegions.py
+++ Breadth_First_Search/surroundedRegions.py
@@ -120,6 +120,45 @@ c = [list(i) for i in a]
 x = Solution_secondround()
 print x.solve(c)
 
+from collections import deque
+class Solution_3rd:
+    # @param board, a 2D array
+    # Capture all regions by modifying the input board in-place.
+    # Do not return any value.
+    def solve(self, board):
+        m, n = len(board), len(board[0])
+        visited = [ [False for i in range(n)] for j in range(m)]
+        components = deque()
+        def valid(i,j):
+            if i>=0 and i<m and j>=0 and j<n and not visited[i][j] and board[i][j] == 'O':
+                visited[i][j] = True
+                return True
+            return False
+        for i in range(m):
+            if board[i][0] == 'O' and not visited[i][0]:
+                visited[i][0] = True
+                components.append((i,0))
+            if board[i][n-1] == 'O' and not visited[i][n-1]:
+                visited[i][n-1] = True
+                components.append((i,n-1))
+        for i in range(n):
+            if board[0][i] == 'O' and not visited[0][i]:
+                visited[0][i] = True
+                components.append((0,i))
+            if board[m-1][i] =='O' and not visited[m-1][i]:
+                visited[m-1][i] = True
+                components.append((m-1,i))
+        while components:
+                cur = components.popleft()
+                p,q = cur[0], cur[1]
+                if valid(p-1,q): components.append((p-1,q))
+                if valid(p+1,q): components.append((p+1,q))
+                if valid(p,q-1): components.append((p,q-1))
+                if valid(p,q+1): components.append((p,q+1))
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O' and not visited[i][j]:
+                    board[i][j] = 'X'
 
 
 
