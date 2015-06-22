d36e301de993f4d03daf22db94aed84404843a17
Effy Huang <effy.yao@gmail.com>
2015-06-22 14:27:43 -0700

    added candy

--- greedy/candy.py
+++ greedy/candy.py
@@ -53,3 +53,17 @@ class Solution_secondround:
             if ratings[i]>ratings[i+1]:
                 right[i] = max(left[i], right[i+1]+1)
         return sum(right)
+
+
+class Solution_3rd:
+    # @param ratings, a list of integer
+    # @return an integer
+    def candy(self, ratings):
+        candies = [1 for i in range(len(ratings))]
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1]+1
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1]+1)
+        return sum(candies)
